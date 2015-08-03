d1948ea3c631ad34106523d8a8bdcc0d2b8e2777
initt <init.1225@gmail.com>
2015-08-03 19:53:19 +0900

    32bit OS対応

--- git-archive-diff.bat
+++ git-archive-diff.bat
@@ -4,7 +4,11 @@ cd %appdata%\..\local\Atlassian\SourceTree\git_local\
 SET REPO=%1
 SET NEW_SHA=%2
 SET OLD_SHA=%3
-SET COMSPEC=%WINDIR%\SysWOW64\cmd.exe
+
+if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+  SET COMSPEC=%WINDIR%\SysWOW64\cmd.exe
+)
+
 SET BIN=bin
 
 if NOT EXIST %REPO%\nul goto EOF
